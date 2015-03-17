5e3abc5863e659339b0e133a230b09dd72281d24
Doug Voss <douglas.c.voss@gmail.com>
2015-03-16 19:33:24 -0600

    Added printIntroductions and countDownTillIntroduction functions

--- IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
@@ -18,13 +18,36 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
+    NSString *myName = @"Doug Voss";
     
+    [self countDownTillIntroduction:4];
+    [self printIntroductions:myName];
     
     return YES;
 }
 
 // Define new functions here...
+- (void)printIntroductions:(NSString *)name
+{
+    NSString *introduction = [NSString stringWithFormat:@"My name is %@", name];
+    NSString *japaneseIntro = [name stringByAppendingString:@" to moushimasu"];
+    
+    NSLog(@"%@", introduction);
+    NSLog(@"%@", japaneseIntro);
+}
 
-
+- (void)countDownTillIntroduction:(int)numberOfDays
+{
+    if (numberOfDays==0)
+    {
+        NSLog(@"The time has come");
+    }
+    else
+    {
+        int oneLessDay = numberOfDays - 1;
+        NSLog(@"%d days left until introductions", numberOfDays);
+        [self countDownTillIntroduction:oneLessDay];
+    }
+}
 
 @end
